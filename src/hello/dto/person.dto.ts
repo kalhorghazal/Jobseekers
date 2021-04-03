d3fe5052c94a50d41cff3ff6a37fbaf93c2f3b20
Ghazal <kalhorghazal1378@gmail.com>
2021-04-03 13:13:59 +0430

    Initial commit

---
+++ src/hello/dto/person.dto.ts
@@ -0,0 +1,15 @@
+import { Length, IsOptional, Min, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class PersonDto {
+    @Length(3, 10)
+    @ApiProperty({description: 'Enter Your Name > ', minLength: 3, default: 'Ghazal', maxLength: 10})
+
+    name: string;
+
+    @IsNumber()
+    @IsOptional()
+    @Min(1960)
+    @ApiPropertyOptional({ description: 'Optional', default:1999, minimum:1960 })
+    year: number;
+}
